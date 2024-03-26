1c1a8ddea77ed095d0aff767d10eaa7782ea10f6
WP2-HandanuAkmalFauzan <handanua2f3@gmail.com>
2024-03-26 20:27:46 +0700

    First Commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama Saya Handanu Akmal Fauzan, 
+        Saya saat ini tinggal di bumi, 
+        kegiatan yang saya sukai adalah nonton dan membaca";
+    }
+}
