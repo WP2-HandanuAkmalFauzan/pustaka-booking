f6c0d66a36262576808ed9fdea3ee494e8560732
WP2-HandanuAkmalFauzan <164021971+WP2-HandanuAkmalFauzan@users.noreply.github.com>
2024-05-09 17:50:00 +0700

    Pertemuan 4

--- application/views/v_about.php
+++ application/views/v_about.php
@@ -2,8 +2,8 @@
     <h1><?php echo $judul ?></h1>
     <h4>Nama</h4>
     <ul type="disc">
-        <li>Nama Depan : Imam</li>
-        <li>Nama Belakang : Nawawi</li>
+        <li>Nama Depan      : Imam</li>
+        <li>Nama Belakang   : Nawawi</li>
     </ul>
     <br>
     <h4>Alamat</h4>
--- application/views/v_index.php
+++ application/views/v_index.php
@@ -1,7 +1,7 @@
 <section>
             <h1><?php echo $judul ?></h1>
             <p align='justify'>Pada pengertian codeigniter di atas 
-tadi di jelaskan bahwa codeigniter menggunakan metode MVC. Apa itu 
+tadi dijelaskan bahwa codeigniter menggunakan metode MVC. Apa itu 
 MVC? Kita juga harus mengetahui apa itu MVC sebelum masuk dan lebih 
 jauh dalam belajar codeigniter.</p>
             <p>MVC adalah teknik atau konsep yang memisahkan 
